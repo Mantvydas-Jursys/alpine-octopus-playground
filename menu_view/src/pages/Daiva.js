58e1ba853fb4031f2604eac135d34b6b3da437fc
DaivaGi <daiva.gik@gmail.com>
2023-02-04 01:02:35 +0200

    testas

--- menu_view/src/pages/Daiva.js
+++ menu_view/src/pages/Daiva.js
@@ -1,8 +1,29 @@
+import Button from 'react-bootstrap/Button';
+import OverlayTrigger from 'react-bootstrap/OverlayTrigger';
+import Popover from 'react-bootstrap/Popover';
 
 export function Daiva() {
+    const popover = (
+        <Popover id="popover-basic">
+          <Popover.Header as="h3">Popover right</Popover.Header>
+          <Popover.Body>
+            And here's some <strong>amazing</strong> content. It's very engaging.
+            right?
+          </Popover.Body>
+        </Popover>
+      );
+      
+      const Example = () => (
+        <OverlayTrigger trigger="click" placement="right" overlay={popover}>
+          <Button variant="success" className='m-5'>Click me to see</Button>
+        </OverlayTrigger>
+      );
+      
+      
+    
     return(
         <div>
-Daiva
+<Example />
         </div>
     )
     
