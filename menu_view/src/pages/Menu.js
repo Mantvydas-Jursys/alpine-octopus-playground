ae086846d3de194af9a385d86cdab35620b074df
unknown <DaivaGi>
2023-02-04 00:40:23 +0200

    pataisymai

--- menu_view/src/pages/Menu.js
+++ menu_view/src/pages/Menu.js
@@ -1,14 +1,16 @@
+import { Button } from "react-bootstrap";
+
 import { Link } from "react-router-dom";
 
 export function Menu() {
     return(
-        <>        
-        <button><Link to={"/ingrida"}>Ingrida</Link></button>
-        <button><Link to={"/daiva"}>Daiva</Link></button>
-        <button><Link to={"/mantas" }>Mantas</Link></button>
-        <button><Link to={"/mantvydas" }>Mantvydas</Link></button>
-        <button><Link to={"/vilmantas" }>Vilmantas</Link></button>
-        <button><Link to={"/vladislav" }>Vladislav</Link></button>
+        <>           
+        <Link to={"/ingrida"}><Button className="m-2 ms-5">Ingrida</Button></Link>
+        <Link to={"/daiva"}><Button className="m-2">Daiva</Button></Link>
+        <Link to={"/mantas" }><Button className="m-2">Mantas</Button></Link>
+        <Link to={"/mantvydas" }><Button className="m-2">Mantvydas</Button></Link>
+        <Link to={"/vilmantas" }><Button className="m-2">Vilmantas</Button></Link>
+        <Link to={"/vladislav" }><Button className="m-2">Vladislav</Button></Link>        
         </>
     )
 }
